b4c281249da205bdb15f003b578b77a518d699f3
Amir Kooshky <kooshkya@gmail.com>
2024-07-21 10:43:13 +0330

    chore: add the main application

--- Articles/settings.py
+++ Articles/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'articles_site',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
