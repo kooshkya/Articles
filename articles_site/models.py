12aa35f260f297fe0816940e856dd397cf6bcdbc
Amir Kooshky <kooshkya@gmail.com>
2024-07-21 11:20:07 +0330

    feat: create models for article and rating

--- articles_site/models.py
+++ articles_site/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    ratings = models.ManyToManyField(get_user_model(), through='Rating', related_name='rated_articles')
+
+
+class Rating(models.Model):
+    RATING_CHOICES = [
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    class Meta:
+        unique_together = ('user', 'article')
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=RATING_CHOICES)
