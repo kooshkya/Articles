33c959f4b510afea75dc2096010d93997c5a5918
Amir Kooshky <kooshkya@gmail.com>
2024-07-22 16:38:01 +0330

    feat: add admin site for article and rating

--- articles_site/admin.py
+++ articles_site/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Article, Rating
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+    extra = 1
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'author')
+    list_filter = ('author',)
+    inlines = [RatingInline]
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('article', 'user', 'rating')
+    list_filter = ('rating', 'user', 'article')
+    search_fields = ('user__username', 'article__title')
+    autocomplete_fields = ['user', 'article']
+
